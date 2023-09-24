ae874ae282c2b7fe5b42a6811627f1270bddeffc
Dspofu <82218878+dspofu@users.noreply.github.com>
2023-09-24 15:50:05 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
-# [<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="35">](https://github.com/dspofu/managerDB) ManagerDB [<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/2560px-Npm-logo.svg.png" alt="NPM" width="40">](https://www.npmjs.com/package/managerDB)
+# [<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="35">](https://github.com/dspofu/managerDB) ManagerDB [<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/2560px-Npm-logo.svg.png" alt="NPM" width="46">](https://www.npmjs.com/package/managerDB)
 
 Um simples gerenciador de json como "data base(DB)" local em sua aplicação bem __avançado__ e __maleável__ para usar como quiser.
 
-- Compatível com ES5 & ES6</li>
+- Compatível com ES5 & ES6
 - Allowed to use "require()" and "import"
 
 ```js
 const db = new ManagerDB();
 ```
 
-Você pode escolher o espacamendo colocando o numero de espaço dentro da classe `ManagerDB`, sendo o `padrão: 0`
+>Você pode escolher o espacamendo colocando o numero de espaço dentro da classe `ManagerDB`, sendo o `padrão: 0`
+
 
 ```js
 new ManagerDB(2); // Espaçamento adicional
+```
+
+>Você tambem poderá optar entre um `json` armazenando "base64" ou "utf-8" que é o padrão. Para "uft-8" não insira um segundo parâmetro.
+
+```js
+new ManagerDB(0, "base64"); // Codificação opicional
 ```
 
 ## Metodos
@@ -36,12 +43,17 @@ console.log(db.path("./dir").get())
 Passe a chave *"key"* de sua preferencia e o valor *"value"*
 
 ```js
-db.path("./dir").set("key", "value");
+db.path("./dir").set("key", "value"); // Criando por parametro.
+db.path("./dir").set({"key1": "value"}); // Formatação objeto.
+db.path("./dir").set({"key2": "value", "key3": "value"}); // Formatação com objetos.
 ```
 #### Resultado `exemplo`
 ```js
   {
-    "key": "value"
+    "key": "value", // Criando por parametro.
+    "key1": "value", // Formatação objeto.
+    "key2": "value", // Formatação com objetos.
+    "key3": "value", // Formatação com objetos.
   }
 ```
 ## 
--- managerDB.cjs
+++ managerDB.cjs
@@ -1,17 +1,24 @@
 const { readFileSync, writeFileSync, existsSync } = require("node:fs")
 
-function compact(param) {
+function utf8(param) {
   let space;
   if (!param.space) {space = 0} else {space = param.space}
     return {
-        get: () => param.get,
+        get: () => param.get,// fazer uma busrca por "key"
         /**
          * @param {any} key - example (...: "value")
          * @param {any} value - value: example ("key": "...")
          */
         set: (key, value) => {
             if (!key || value === undefined && typeof key !== 'object') throw new Error("Modo de uso: \"key\", \"value\"");
-            if (typeof key == 'object') return writeFileSync(param.path, JSON.stringify(key, null, space));
+            if (typeof key == 'object') {
+              let values = [];
+              Object.values(key).map((param)=>values.push(param))
+              return Object.keys(key).map((keys, index)=>{
+                param.get[keys] = values[index]
+                writeFileSync(param.path, JSON.stringify(param.get, null, space));
+              })
+            }
             param.get[key] = value
             writeFileSync(param.path, JSON.stringify(param.get, null, space));
         },
@@ -27,33 +34,87 @@ function compact(param) {
         clear: () => { writeFileSync(param.path, "{}"); }
     }
 }
+
+function base64(param) {
+  let space;
+  if (!param.space) {space = 0} else {space = param.space}
+    return {
+        get: () => {
+          let obj = {}
+          let keys = []
+          Object.keys(param.get).map((value) => keys.push(value))
+          Object.values(param.get).map((value, index) => ({ [keys[index]]: Buffer.from(value, 'base64').toString("utf-8") }) ).map((value, index) => {Object.assign(obj, value) })
+          return obj
+        },
+        /**
+         * @param {any} key - example (...: "value")
+         * @param {any} value - value: example ("key": "...")
+         */
+        set: (key, value) => {
+            if (!key || value === undefined && typeof key !== 'object') throw new Error("Modo de uso: \"key\", \"value\".");
+            if (value !== undefined && typeof value !== 'string') throw new Error("Só passe \"string\" no \"value\".");
+            if (typeof key == 'object') {
+              let values = [];
+              Object.values(key).map((param) => {
+                if (typeof param !== 'string') throw new Error("Só passe \"string\" no \"value\".");
+                return values.push(param);
+              })
+              return Object.keys(key).map((keys, index) => {
+                param.get[keys] = Buffer.from(values[index]).toString("base64");
+                writeFileSync(param.path, JSON.stringify(param.get, null, space));
+              })
+            }
+
+            param.get[key] = Buffer.from(value).toString("base64")
+            writeFileSync(param.path, JSON.stringify(param.get, null, space));
+        },
+        /**
+         * @param {*} key - Use: "key" para apagar um item especifico.
+         */
+        delete: (key) => {
+            if (!key) throw new Error("Obrigatório o uso de uma chave: \"key\"");
+            if (typeof key !== "string") throw new Error("String não identificada.");
+            delete param.get[key]
+            writeFileSync(param.path, Buffer.from(JSON.stringify(param.get, null, space)).toString("utf-8"));
+        },
+        clear: () => { writeFileSync(param.path, ""); }
+    }
+}
+
+function modify(dir, space, codifyType) {
+  if (!existsSync(dir)) throw new Error("Caminho não encontrado.");
+  if (!readFileSync(dir, 'utf-8')) writeFileSync(dir, '{}');
+  if (!["utf-8", "base64"].includes(codifyType) && codifyType) throw new Error("Os tipos de valodação de dados são: \"utf-8\" ou \"base64\"");
+  
+  if (!codifyType || codifyType == "utf-8") {
+     return utf8({
+      get: JSON.parse(readFileSync(dir, 'utf-8')), // Conteudo do json
+      path: dir, // Caminho do arquivo
+      space // Espaçamento do Json
+    });
+  } else { // base64
+    return base64({
+      get: JSON.parse(readFileSync(dir, 'utf-8')),//Buffer.from(readFileSync(dir, 'utf-8'), 'base64').toString('utf-8'),
+      path: dir,
+      space
+    });
+  }
+}
+
 class ManagerDB {
   /**
-  * @param {Number} space - Controle do espaçamento do json.
+  * @param {Number} space - Controle do espaçamento do json. Padão: "0"
+  * @param {string} codifyType - Escrita e transcrita de "base64". Padrão: "utf-8"
   */
-    constructor(space) {
+    constructor(space, codifyType) {
       if (typeof space !== "number" && space) throw new Error(`Tenho certeza de que \"${space}\" não é um numero.`);
+      if (typeof codifyType !== "string" && codifyType && codifyType == "") throw new Error(`O tipo: \"${codifyType}\" não é aceito, procure algo como (\"base64\" ).`);
       /**
-       * 
        * @param {string} dir - Diretório raiz do JSON.
        * @returns 
        */
-      this.path=(dir)=>{
-          if (!existsSync(dir)) { throw new Error("Caminho não encontrado."); }
-          else if (!readFileSync(dir, 'utf-8')) {
-            writeFileSync(dir, '{}')
-            return compact({
-              get: JSON.parse(readFileSync(dir, 'utf-8')),
-              path: dir,
-              space
-            })
-          }
-          else return compact({
-            get: JSON.parse(readFileSync(dir, 'utf-8')), // Conteudo do json
-            path: dir, // Caminho do arquivo
-            space // Espaçamento do Json
-        });
-      }
+      this.path = (dir) => modify(dir, space, codifyType);
     }
 }
+
 module.exports = { ManagerDB }
