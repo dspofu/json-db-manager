f1f8ccb12bfba7a832e94706b2aef17afa620140
Dspofu <82218878+dspofu@users.noreply.github.com>
2023-10-10 14:32:08 -0300

    Update error.js
    planning new layout

--- modules/html/error.js
+++ modules/html/error.js
@@ -4,34 +4,36 @@ exports.htmlError = void 0;
 const htmlError = `<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error</title>
-</head>
-<style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Error</title>
+  <style>
     body {
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        background-color: rgb(232, 232, 232);
-        text-align: center;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      background-color: rgb(232, 232, 232);
+      text-align: center;
     }
 
-    .error-container {
-        margin-top: 100px;
-        padding: 20px;
-        background-color: rgba(255, 255, 255, 0.323);
-        border-radius: 8px;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    .container {
+      margin-top: 25vh;
+      padding: 20px;
+      background-color: rgba(255, 255, 255, 0.323);
+      border-radius: 12px;
+      box-shadow: 0px 0px 12px 12px rgb(0 0 0 / 20%);
     }
 
     h1 { color: #ff0000; }
-</style>
+
+    a { text-decoration: none; }
+  </style>
 </head>
 <body>
-<div class="error-container">
+  <div class="container">
     <h1>Erro 404</h1>
-    <p>Sorry, the page you are looking for was not found.</p>
+    <p>Opss! The page you are looking for was not found</p><img
+      src="" width="25%" alt="Image is not found">
     <p><a href="/data">Return to home page</a></p>
-</div>
+  </div>
 </body>
 </html>`;
 exports.htmlError = htmlError;
